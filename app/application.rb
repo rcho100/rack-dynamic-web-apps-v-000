7691692369c2be32b52a56bb6fffa31f5ee2a87b
Richard Cho <richardcho100@gmail.com>
2019-03-14 15:36:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,12 @@ class Application
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
-    
+
+    if num_1 == num_2 && num_2 == num_3
+      resp.write "You win"
+    else
+      resp.write "You lose"
+    end
     resp.write "Hello, World"
     resp.finish
   end
